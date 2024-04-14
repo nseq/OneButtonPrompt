a6d269fc8f6be926735b508fe95c4fbc78e20fb2
nseq <30580442+nseq@users.noreply.github.com>
2024-04-14 14:56:25 +0300

    Update model_lists.py

--- model_lists.py
+++ model_lists.py
@@ -86,6 +86,10 @@ def get_samplers_for_img2img():
     return samplerlist
 
 def upscalers_on_startup():
+    try:
+        modelloader.cleanup_models()
+    except:
+        pass
     modelloader.load_upscalers()
     upscalerlistfromwWebUI = [x.name for x in shared.sd_upscalers]
 
